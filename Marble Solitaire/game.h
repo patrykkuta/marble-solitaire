4ccb5bbe65571a9ddbbb0292c495b71117f58a42
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-22 15:57:40 +0000

    Move board and difficulty to the Game class

---
+++ Marble Solitaire/game.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stack>
+#include <queue>
+#include "board_configuration.h"
+#include "difficulty.h"
+#include "move.h"
+
+class Game {
+private:
+	int board[7][7];
+	std::stack<Move> recentMoves;
+	unsigned int movesCounter = 0;
+	unsigned int difficulty;
+
+public:
+	Game(int(&configuration)[7][7], DIFFICULTY difficulty);
+	auto getBoard() -> int(*)[7];
+	std::stack<Move>* getRecentMoves();
+	unsigned int* getMovesCounter();
+	bool move(Move move);
+	void undoMove();
+	std::queue<Move> solve();
+	Move getHint();
+	bool hasWon();
+	void setDifficulty(DIFFICULTY difficulty);
+};
+
