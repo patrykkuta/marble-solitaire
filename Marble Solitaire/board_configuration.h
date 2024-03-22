23e1e12296b30c1d534064ba80b31de0e02550e0
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-22 12:00:33 +0000

    Create BOARD_CONFIGURATION header

---
+++ Marble Solitaire/board_configuration.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_CONFIGURATION_H
+#define BOARD_CONFIGURATION_H
+
+class BOARD_CONFIGURATION {
+public:
+    static int DIAMOND[7][7];
+    static int FULL[7][7];
+    static int CROSS[7][7];
+    static int PLUS[7][7];
+    static int TRIANGLE[7][7];
+};
+
+#endif // BOARD_CONFIGURATION_H
