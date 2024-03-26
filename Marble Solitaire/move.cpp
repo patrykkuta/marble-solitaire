32c3c25e538605e31b542249bcc835173402035e
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-26 12:22:10 +0000

    Return address of members

--- Marble Solitaire/move.cpp
+++ Marble Solitaire/move.cpp
@@ -3,9 +3,10 @@
 
 Move::Move(std::pair<short, short> origin, std::pair<short, short> destination) : origin(origin), destination(destination) {}
 std::pair<short, short> Move::getOrigin() {
+std::pair<short, short>& Move::getOrigin() {
 	return origin;
 }
 
-std::pair<short, short> Move::getDestination() {
+std::pair<short, short>& Move::getDestination() {
 	return destination;
 }
