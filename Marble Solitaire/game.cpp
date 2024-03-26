ddcb3005a54cf5a18f4d82fa111ed4c4c2ddcddd
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-26 12:17:04 +0000

    Implement the performMove method

--- Marble Solitaire/game.cpp
+++ Marble Solitaire/game.cpp
@@ -26,35 +26,20 @@ std::stack<Move>* Game::getRecentMoves() {
 unsigned int* Game::getMovesCounter() {
 	return &movesCounter;
 }
-bool Game::move(Move move) {
+bool Game::performMove(Move move) {
     // Extract origin and destination coordinates from the move
     int originY = move.getOrigin().first;
     int originX = move.getOrigin().second;
-    int destY = move.getDestination().first;
-    int destX = move.getDestination().second;
+    int destinationY = move.getDestination().first;
+    int destinationX = move.getDestination().second;
 
-    /**
-    Check if the move is valid
-    - distance equal to abs(2)
-    - move only horizontally and vertically, not diagonally 
-    */
-    int dY = destY - originY;
-    int dX = destX - originX;
-    if ((dX != 0 && dY != 0) || abs(dX) != 2 || abs(dY) != 2) {
-        return false;
-    }
+    if (!isMoveValid(originY, originX, destinationY, destinationX)) return false;
 
-    // Check if there's a marble at the origin and an empty space at the destination
-    // 1 - piece, 0 - space, -1 - wall
-    int originValue = board[originY][originX];
-    int middleValue = board[originY + dY / 2][originX + dX / 2];
-    int destinationValue = board[destY][destX];
-    if (originValue != 1 || middleValue != 1 || destinationValue != 0) {
-        return false;
-    }
+    int dY = destinationY - originY;
+    int dX = destinationX - originX;
 
     // Perform the move
-    board[destY][destX] = 1;
+    board[destinationY][destinationX] = 1;
     board[originY][originX] = 0;
     board[originY + dY / 2][originX + dX / 2] = 0;
 
