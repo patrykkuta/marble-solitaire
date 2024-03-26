57edd5a50b64be535a5071ce94f9ace51ad9f66c
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-26 12:23:00 +0000

    Copy the values of origin and destination

--- Marble Solitaire/move.cpp
+++ Marble Solitaire/move.cpp
@@ -1,8 +1,12 @@
 #include "move.h"
 #include <utility>
 
-Move::Move(std::pair<short, short> origin, std::pair<short, short> destination) : origin(origin), destination(destination) {}
-std::pair<short, short> Move::getOrigin() {
+Move::Move(std::pair<short, short> origin, std::pair<short, short> destination) {
+	this->origin.first = origin.first;
+	this->origin.second = origin.second;
+	this->destination.first = destination.first;
+	this->destination.second = destination.second;
+}
 std::pair<short, short>& Move::getOrigin() {
 	return origin;
 }
