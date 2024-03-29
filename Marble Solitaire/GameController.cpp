e5f991d3bc77aed4dd0bf442438697b17459361f
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-29 09:32:24 +0000

    Add methods and members to the GameController class

--- Marble Solitaire/GameController.cpp
+++ Marble Solitaire/GameController.cpp
@@ -1,7 +1,29 @@
 #include "GameController.h"
 
+using namespace std;
+
 GameController::GameController() {}
 
 Game* GameController::newGame(const std::vector<std::vector<int>>& configuration, DIFFICULTY difficulty) {
-	return new Game(configuration, difficulty);
+	game = new Game(configuration, difficulty);
+	return game;
 }
+
+GameController::~GameController() {
+	delete game;
+}
+
+bool GameController::move() {
+	// Take the selected marble
+	// Take the selected space
+	// Construct a Move object with marble and space coordinates converted to array indices of the board
+	// Call the game.performMove(move) to perform the move and return the value the method returns
+	return true;
+}
+
+vector<vector<int>>* GameController::getBoard() {
+	return game->getBoard();
+}
+
+void GameController::setMarble(GLuint marble) { this->selectedMarble = marble; }
+void GameController::setEmptySpace(GLuint emptySpace) { this->selectedEmptySpace = emptySpace; }
--- Marble Solitaire/GameController.h
+++ Marble Solitaire/GameController.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include "game.h"
+#include <vector>
+#include <GL/glew.h>
 class GameController
 {
+private:
+	GLuint selectedMarble;
+	GLuint selectedEmptySpace;
+	Game *game;
 public:
 	GameController();
+	~GameController();
 	Game* newGame(const std::vector<std::vector<int>>& configuration, DIFFICULTY difficulty);
+	bool move();
+	void setMarble(GLuint marble);
+	void setEmptySpace(GLuint emptySpace);
+	std::vector<std::vector<int>>* getBoard();
 };
 
