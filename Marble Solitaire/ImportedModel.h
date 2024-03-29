d805086746c79fd85a38672acfc1c8f96a1e5e82
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-03-29 09:32:57 +0000

    Implement ImportedModel class for representing models

---
+++ Marble Solitaire/ImportedModel.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+
+class ImportedModel
+{
+private:
+	int numVertices;
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> texCoords;
+	std::vector<glm::vec3> normalVecs;
+public:
+	ImportedModel(const char* filePath);
+	int getNumVertices();
+	std::vector<glm::vec3> getVertices();
+	std::vector<glm::vec2> getTextureCoords();
+	std::vector<glm::vec3> getNormals();
+};
