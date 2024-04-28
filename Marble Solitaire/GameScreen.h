1ec5463016dabfa76cae15f1dab1e86b065e0d60
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-04-28 19:19:06 +0100

    Abstract game related functionalities into a GameScreen class

---
+++ Marble Solitaire/GameScreen.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Screen.h"
+#include "ImportedModel.h"
+#include "Camera.h"
+#include <GLFW/glfw3.h>
+#include <vector>
+#include "GameController.h"
+#include "Framebuffer.h"
+#include "Texture.h"
+#include "Shader.h"
+
+using namespace std;
+
+class GameScreen : public Screen {
+public:
+	GameScreen();
+	~GameScreen();
+	void render(Window& window) override;
+
+	void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+	void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+private:
+	ImportedModel marbleObj = ImportedModel("marble.obj");
+	ImportedModel boardObj = ImportedModel("board.obj");
+	Shader marbleShaderProgram = Shader("marbleVertex.glsl", "marbleFragment.glsl");
+	Shader boardShaderProgram = Shader("boardVertex.glsl", "boardFragment.glsl");
+	Texture marbleTexture = Texture("marbleTexture.jpg");
+	Texture boardTexture = Texture("boardTexture2.jpg");
+	Framebuffer offScreenFramebuffer;
+	Camera camera = Camera(glm::vec3(0, 0, 4), glm::vec3(60, 0, 0));
+	GameController gameController = GameController();
+
+	GLuint marbleVao[1], boardVao[1]; // renderer class?
+	GLuint marbleVbo[3], boardVbo[3]; // renderer class?
+	int width, height; // renderer class
+	float aspect; // renderer class
+	glm::mat4 pMat; // renderer class
+
+	glm::vec3 marblesPositions[7][7] = {
+		{glm::vec3(0,0,0), glm::vec3(0,0,0), glm::vec3(-0.275841, 0.17, -0.829844), glm::vec3(0.00116, 0.17, -0.829844), glm::vec3(0.278161, 0.17, -0.829844), glm::vec3(0,0,0), glm::vec3(0,0,0)},
+		{glm::vec3(0,0,0), glm::vec3(0,0,0), glm::vec3(-0.275841, 0.17, -0.552842), glm::vec3(0.00116, 0.17, -0.552842), glm::vec3(0.278161, 0.17, -0.552842), glm::vec3(0,0,0), glm::vec3(0,0,0)},
+		{glm::vec3(-0.829844, 0.17, -0.275841), glm::vec3(-0.552842, 0.17, -0.275841), glm::vec3(-0.275841, 0.17, -0.275841), glm::vec3(0.00116, 0.17, -0.275841), glm::vec3(0.278161, 0.17, -0.275841), glm::vec3(0.555163, 0.17, -0.275841), glm::vec3(0.832164, 0.17, -0.275841)},
+		{glm::vec3(-0.829844, 0.17, 0.00116), glm::vec3(-0.552842, 0.17, 0.00116), glm::vec3(-0.275841, 0.17, 0.00116), glm::vec3(0.00116, 0.17, 0.001161), glm::vec3(0.278161, 0.17, 0.00116), glm::vec3(0.555163, 0.17, 0.00116), glm::vec3(0.832164, 0.17, 0.00116)},
+		{glm::vec3(-0.829844, 0.17, 0.278162), glm::vec3(-0.552842, 0.17, 0.278162), glm::vec3(-0.275841, 0.17, 0.278162), glm::vec3(0.00116, 0.17, 0.278162), glm::vec3(0.278161, 0.17, 0.278162), glm::vec3(0.555163, 0.17, 0.278162), glm::vec3(0.832164, 0.17, 0.278162)},
+		{glm::vec3(0,0,0), glm::vec3(0,0,0), glm::vec3(-0.275841, 0.17, 0.555163), glm::vec3(0.00116, 0.17, 0.555163), glm::vec3(0.278161, 0.17, 0.555163), glm::vec3(0,0,0), glm::vec3(0,0,0)},
+		{glm::vec3(0,0,0), glm::vec3(0,0,0), glm::vec3(-0.275841, 0.17, 0.832164), glm::vec3(0.00116, 0.17, 0.832164), glm::vec3(0.278161, 0.17, 0.832164), glm::vec3(0,0,0), glm::vec3(0,0,0)}
+	}; // pass to the renderer class?
+
+	void drawSceneForColorPicking(); // renderer class?
+	void setupVertices(ImportedModel model, GLuint vao[], GLuint vbo[]); // renderer class?
+	void displayBoard(GLFWwindow* window, double currentTime); // renderer class?
+
+};
+
