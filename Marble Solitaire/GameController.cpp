bae0d627eae341e8f0220cd4e2ddabdfca8c941e
Patryk Kuta <77497842+patryk9837@users.noreply.github.com>
2024-04-28 19:17:15 +0100

    Implement GameController functions

--- Marble Solitaire/GameController.cpp
+++ Marble Solitaire/GameController.cpp
@@ -5,25 +5,33 @@ using namespace std;
 GameController::GameController() {}
 
 Game* GameController::newGame(const std::vector<std::vector<int>>& configuration, DIFFICULTY difficulty) {
+	if (game != nullptr) delete game;
+
 	game = new Game(configuration, difficulty);
 	return game;
 }
 
 GameController::~GameController() {
-	delete game;
+	if (game != nullptr) delete game;
 }
 
 bool GameController::move() {
-	// Take the selected marble
-	// Take the selected space
-	// Construct a Move object with marble and space coordinates converted to array indices of the board
-	// Call the game.performMove(move) to perform the move and return the value the method returns
-	return true;
+	Move move(selectedMarble, selectedEmptySpace);
+	bool moved = game->performMove(move);
+
+	if (game->hasFinished()) {
+		delete game;
+		game = new Game(BOARD_CONFIGURATION::PLUS, DIFFICULTY::EASY);
+	}
+
+	return moved;
 }
 
 vector<vector<int>>* GameController::getBoard() {
 	return game->getBoard();
 }
 
-void GameController::setMarble(GLuint marble) { this->selectedMarble = marble; }
-void GameController::setEmptySpace(GLuint emptySpace) { this->selectedEmptySpace = emptySpace; }
+void GameController::setMarble(pair<short, short> marble) { this->selectedMarble = marble; }
+void GameController::setEmptySpace(pair<short, short> emptySpace) { this->selectedEmptySpace = emptySpace; }
+pair<short, short> GameController::getMarble() { return selectedMarble; }
+pair<short, short> GameController::getEmptySpace() { return selectedEmptySpace; }
--- Marble Solitaire/GameController.h
+++ Marble Solitaire/GameController.h
@@ -3,19 +3,22 @@
 #include "game.h"
 #include <vector>
 #include <GL/glew.h>
+
 class GameController
 {
 private:
-	GLuint selectedMarble;
-	GLuint selectedEmptySpace;
+	std::pair<short, short> selectedMarble = {-1, -1};
+	std::pair<short, short> selectedEmptySpace = { -1, -1 };
 	Game *game;
 public:
 	GameController();
 	~GameController();
 	Game* newGame(const std::vector<std::vector<int>>& configuration, DIFFICULTY difficulty);
 	bool move();
-	void setMarble(GLuint marble);
-	void setEmptySpace(GLuint emptySpace);
+	void setMarble(std::pair<short, short> marble);
+	void setEmptySpace(std::pair<short, short> emptySpace);
 	std::vector<std::vector<int>>* getBoard();
+	std::pair<short, short> getMarble();
+	std::pair<short, short> getEmptySpace();
 };
 
